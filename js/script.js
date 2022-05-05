9881da7f3f9bbadbf201b4d63644b6d08b041a4b
Kardast <dottorsf@gmail.com>
2022-05-05 10:08:51 +0200

    Code Improvements

--- js/script.js
+++ js/script.js
@@ -12,27 +12,25 @@ const priceKm = distanceKm * 0.21;
 console.log(priceKm);
 
 // Se utente è minorenne (-18 anni) ha diritto a uno sconto del 20% 
-const young = priceKm * 0.20;
-console.log("young è: " + young);
 
 // Se utente over 65 anni ha diritto a sconto del 40%
-const old = priceKm * 0.40;
-console.log("old è: " + old);
 
 let finalPrice = priceKm;
 console.log(finalPrice);
 
 // Conteggio sconti o non
 if (userAge < 18) {
+    const young = priceKm * 0.20;
+    console.log("young è: " + young);
     finalPrice = priceKm - young;
     console.log("Il costo è " + (priceKm - young));
 
 } else if (userAge > 65) {
+    const old = priceKm * 0.40;
+    console.log("old è: " + old);
     finalPrice = priceKm - old;
     console.log("Il costo è " + (priceKm - old));
 }
 
-finalPrice = finalPrice.toFixed(2);
-
 // Far dire alla macchina quanto spende l'utente
-document.getElementById("mio_id").innerHTML = "Il costo del suo biglietto è " + finalPrice + "€";
+document.getElementById("mio_id").innerHTML = "Il costo del suo biglietto è " + finalPrice.toFixed(2) + "€";
